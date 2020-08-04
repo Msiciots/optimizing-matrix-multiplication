9b2d72966cfd84519c8747380e465d2e47d9ab81
Msiciots <f74049020@gs.ncku.edu.tw>
2020-08-05 07:56:52 +0800

    Make a traditional method

---
+++ tradition.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char* argv[]){
+
+    FILE *f = fopen(argv[1], "r");
+	FILE *out = fopen("output/tradition_output", "w");
+    int array_size;
+	// read first matrix
+	fscanf(f,"%d",&array_size);
+    int **first=(int **)malloc(array_size*sizeof(int *));
+    for(int i=0;i<array_size;i++)first[i]=(int *)malloc(array_size*sizeof(int *));
+    for(int i=0;i<array_size;i++){
+	    for(int j=0;j<array_size;j++){
+      		fscanf(f,"%d",&first[i][j]);
+		}
+	}
+    //read second matrix
+    fscanf(f,"\n");
+    int **second=(int **)malloc(array_size*sizeof(int *));
+    for(int i=0;i<array_size;i++)second[i]=(int *)malloc(array_size*sizeof(int *));
+
+    for(int i=0;i<array_size;i++){
+	    for(int j=0;j<array_size;j++){
+      		fscanf(f,"%d",&second[i][j]);
+		}
+	}
+    fclose(f);
+    // output matrix
+    int **output=(int **)malloc(array_size*sizeof(int *));
+    for(int i=0;i<array_size;i++)output[i]=(int *)malloc(array_size*sizeof(int *));
+
+    // debug
+    // for(int i = 0; i < array_size; i++){
+    //     for (int j = 0; j < array_size; j++){
+    //         printf("%d ",first[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    // printf("\n");
+    // for(int i = 0; i < array_size; i++){
+    //     for (int j = 0; j < array_size; j++){
+    //         printf("%d ",second[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    //start clock
+    clock_t start, end;
+    start = clock();
+    //multiply
+    long long int sum = 0;
+    for (int i = 0; i < array_size; i++) {
+       for (int j = 0; j < array_size; j++) {
+         for (int k = 0; k < array_size; k++) {
+           sum = sum + first[i][k]*second[k][j];
+        }
+        output[i][j] = sum;
+        sum = 0;
+      }
+    }
+    //end clock
+    end = clock();
+
+    fprintf(out,"\nElapsed time: %f\n",(double)(end-start)/CLOCKS_PER_SEC);
+
+	for (int i = 0; i < array_size; i++) {
+      for (int j = 0; j < array_size; j++) {
+		  fprintf(out,"%d ",output[i][j]);
+	}
+	fprintf(out,"\n");
+	}
+
+    printf("Elapsed time: %f\n",(double)(end-start)/CLOCKS_PER_SEC);
+	fprintf(out,"\nElapsed time: %f\n",(double)(end-start)/CLOCKS_PER_SEC);
+    return 0;
+}
